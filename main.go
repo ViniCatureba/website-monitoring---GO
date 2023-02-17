4ab9285100ab0461098a103b718f4a7cceb07b14
ViniCatureba <vinicius.bigao14@gmail.com>
2023-02-17 10:23:49 -0300

    Fixing bugs

--- main.go
+++ main.go
@@ -1,29 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
-
-	//"io/ioutil"
-	"bufio"
-	"strconv"
 )
 
-const monitoramentos = 3
-const delay = 10
+const monitoramentos = 4
+const delay = 5
 
 func main() {
-
 	exibeIntroducao()
+
 	for {
 		exibeMenu()
 
-		comando := lerComando()
+		comando := leComando()
 
 		switch comando {
 		case 1:
@@ -39,80 +37,75 @@ func main() {
 			os.Exit(-1)
 		}
 	}
+
 }
 
 func exibeIntroducao() {
 	nome := "Douglas"
-	versao := 1.1
-	fmt.Println("Ola, sr.", nome)
-	fmt.Println("Esse programa esta na versao", versao)
+	versao := 1.2
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
 }
 
 func exibeMenu() {
-	fmt.Println("1- Iniciar Mnitoramento")
+	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
 }
 
-func lerComando() int {
+func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
 	return comandoLido
 }
 
 func iniciarMonitoramento() {
-	fmt.Println("Monitoriando...")
-
-	/* var sites [4]string
-	sites[0] = "https://random-status-code.herokuapp.com/"
-	sites[1] = "https://www.alura.com.br"
-	sites[2] = "https://www.caelum.com.br" */ //arrays
-
-	//slices
-	/* sites := []string{"https://random-status-code.herokuapp.com/", "https://www.alura.com.br", "https://www.caelum.com.br"} */
-
-	sites := lerSitesDoArquivo()
+	fmt.Println("Monitorando...")
+	sites := leSitesDoArquivo()
 
 	for i := 0; i < monitoramentos; i++ {
 		fmt.Printf("--------------------------------------------------Monitoramento(%v)--------------------------------------------------\n\n", i)
 		for i, site := range sites {
-			fmt.Print("Testando site: ", i, ": ", site, "\n\n")
+			fmt.Println("Testando site", i, ":", site)
 			testaSite(site)
 		}
-		time.Sleep(monitoramentos * time.Second)
-
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
 	}
+
+	fmt.Println("")
 }
 
 func testaSite(site string) {
-	resposta, err := http.Get(site)
-
+	resp, err := http.Get(site)
+	
 	if err != nil {
 		fmt.Println("Ocorreu um erro:", err)
+		return
 	}
 
-	if resposta.StatusCode == 200 {
-		fmt.Print("Site: ", site, " foi carregado com sucesso\n\n")
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
 		registraLog(site, true)
 	} else {
-		fmt.Print("Site: ", site, " esta com problemas. Status:", resposta.StatusCode, "\n\n")
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
 		registraLog(site, false)
 	}
 }
 
-func lerSitesDoArquivo() []string {
-
+func leSitesDoArquivo() []string {
 	var sites []string
-	arquivos, err := os.Open("sites.txt") //imprime local na memoria
-	/*arquivo, err := ioutil.ReadFile("site.txt") imprime tudo em bytes */
+	arquivo, err := os.Open("sites.txt")
+
 	if err != nil {
 		fmt.Println("Ocorreu um erro:", err)
 	}
 
-	leitor := bufio.NewReader(arquivos)
+	leitor := bufio.NewReader(arquivo)
 	for {
-
 		linha, err := leitor.ReadString('\n')
 		linha = strings.TrimSpace(linha)
 
@@ -121,29 +114,34 @@ func lerSitesDoArquivo() []string {
 		if err == io.EOF {
 			break
 		}
+
 	}
-	arquivos.Close()
+
+	arquivo.Close()
 	return sites
 }
 
 func registraLog(site string, status bool) {
 
 	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
 	if err != nil {
-		fmt.Println("erro: ", err)
+		fmt.Println(err)
 	}
 
-	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "- online " + strconv.FormatBool(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
 }
 
 func imprimeLogs() {
+
 	arquivo, err := ioutil.ReadFile("log.txt")
 
 	if err != nil {
-		fmt.Println("Error: ", err)
+		fmt.Println(err)
 	}
+
 	fmt.Println(string(arquivo))
 
 }
